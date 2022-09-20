30fbeb9d14c7493042abc15395e3a3936af15452
Christoph Hafner <tibius74@gmail.com>
2022-09-20 23:31:33 +0200

    Actions removed from 'FuncTest'.

--- Source/CompileTests/FuncTest.cs
+++ Source/CompileTests/FuncTest.cs
@@ -4,24 +4,6 @@ namespace CompileTests {
 
     public class FuncTest {
 
-        public Action? A0 { get; set; }
-        public Action<string>? A1 { get; set; }
-        public Action<string, string>? A2 { get; set; }
-        public Action<string, string, string>? A3 { get; set; }
-        public Action<string, string, string, string>? A4 { get; set; }
-        public Action<string, string, string, string, string>? A5 { get; set; }
-        public Action<string, string, string, string, string, string>? A6 { get; set; }
-        public Action<string, string, string, string, string, string, string>? A7 { get; set; }
-        public Action<string, string, string, string, string, string, string, string>? A8 { get; set; }
-        public Action<string, string, string, string, string, string, string, string, string>? A9 { get; set; }
-        public Action<string, string, string, string, string, string, string, string, string, string>? A10 { get; set; }
-        public Action<string, string, string, string, string, string, string, string, string, string, string>? A11 { get; set; }
-        public Action<string, string, string, string, string, string, string, string, string, string, string, string>? A12 { get; set; }
-        public Action<string, string, string, string, string, string, string, string, string, string, string, string, string>? A13 { get; set; }
-        public Action<string, string, string, string, string, string, string, string, string, string, string, string, string, string>? A14 { get; set; }
-        public Action<string, string, string, string, string, string, string, string, string, string, string, string, string, string, string>? A15 { get; set; }
-        public Action<string, string, string, string, string, string, string, string, string, string, string, string, string, string, string, string>? A16 { get; set; }
-
         public Func<string>? F0 { get; set; }
         public Func<string, string>? F1 { get; set; }
         public Func<string, string, string>? F2 { get; set; }
